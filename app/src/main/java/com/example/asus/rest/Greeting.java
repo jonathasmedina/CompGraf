86dbbe5f946723be9b8a28853a8365b8efb98e51
Jonathas Leontino Medina <jonathas.medina@ifms.edu.br>
2016-02-04 20:10:36 -0300

    Initial commit

---
+++ app/src/main/java/com/example/asus/rest/Greeting.java
@@ -0,0 +1,21 @@
+package com.example.asus.rest;
+
+/**
+ * Created by Asus on 01/02/2016.
+ */
+
+public class Greeting {
+
+    private String id;
+    private String content;
+
+    public String getId() {
+        return this.id;
+    }
+
+    public String getContent() {
+        return this.content;
+    }
+
+}
+
